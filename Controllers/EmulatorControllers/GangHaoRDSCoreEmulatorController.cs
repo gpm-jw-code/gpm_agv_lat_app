20f7bc0231c6120f1d6c04e4dffde8ab28489419
jinwei69 <ss610349@gmail.com>
2022-09-08 19:22:03 +0800

    ROSCore API 模擬控制器優化

--- Controllers/EmulatorControllers/GangHaoRDSCoreEmulatorController.cs
+++ Controllers/EmulatorControllers/GangHaoRDSCoreEmulatorController.cs
@@ -1,4 +1,5 @@
-﻿using GPM_AGV_LAT_APP.Controllers.GangHaoBotServerController;
+﻿using GangHaoAGV.Models.Order;
+using GPM_AGV_LAT_APP.Controllers.GangHaoBotServerController;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,10 +12,38 @@ namespace GPM_AGV_LAT_APP.Controllers.EmulatorControllers
         [HttpGet("GangHaoOrderStateChange")]
         public async Task<IActionResult> OrderStateChange(string orderID, string State)
         {
+            var order = GetOrderByOrderID(orderID);
+            if (order != null)
+                order.state = State.ToUpper();
+            return Ok(order);
+        }
+
+        [HttpGet("OrderRunningEmulate")]
+        public async Task<IActionResult> OrderRunningEmulate(string orderID)
+        {
+            var order = GetOrderByOrderID(orderID);
+            if (order != null)
+                order.state = "RUNNING";
+            return Ok(order);
+        }
+
+        [HttpGet("OrderFinishedEmulate")]
+        public async Task<IActionResult> OrderFinishedEmulate(string orderID)
+        {
+            var order = GetOrderByOrderID(orderID);
+            if (order != null)
+                order.state = "FINISHED";
+            return Ok(order);
+        }
+
+
+        private OrderDetails GetOrderByOrderID(string orderID)
+        {
             var index = ServerController.orderList.FindIndex(order => order.id == orderID);
             if (index != -1)
-                ServerController.orderList[index].state = State;
-            return Ok();
+                return ServerController.orderList[index];
+            else
+                return null;
         }
     }
 }
